cbc2986e46c689e655f4e1df481712deb8de0fa7
Clemens Mosig <clemens.mosig@uni-leipzig.de>
2025-06-07 00:19:35 +0200

    add try catch

--- infer_ortho_batch.py
+++ infer_ortho_batch.py
@@ -8,23 +8,27 @@ pathlist = sys.argv[1]
 
 deadwodinference = DeadwoodInference(
     config_path=
-    "/net/home/cmosig/projects/deadtreesmodels/deadwood_inference_config.json")
+    "/net/home/cmosig/projects/deadtreesmodels/deadwood_inference_config_new.json")
 
 with open(pathlist) as f:
     files = f.readlines()
 
     for filename in files:
-        print("processing", filename)
-        f = filename.strip()
+        try:
+            print("processing", filename)
+            f = filename.strip()
 
-        outpath = f.split("/")[-1].replace(".tif", "_prediction.gpkg")
+            outpath = f.split("/")[-1].replace(".tif", "_prediction.gpkg")
 
-        if os.path.exists(outpath):
-            print("skipping", filename)
+            if os.path.exists(outpath):
+                print("skipping", filename)
+                continue
+
+
+            polygons = deadwodinference.inference_deadwood(f)
+            save_poly(outpath,
+                      polygons,
+                      crs=rasterio.open(f).crs)
+        except Exception as e:
+            print("error", e)
             continue
-
-
-        polygons = deadwodinference.inference_deadwood(f)
-        save_poly(outpath,
-                  polygons,
-                  crs=rasterio.open(f).crs)
