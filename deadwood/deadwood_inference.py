b3385fad302fe8c8f13f344850db187446d2bde9
Clemens Mosig <clemens.mosig@uni-leipzig.de>
2025-11-12 21:54:30 +0100

    format

--- deadwood/deadwood_inference.py
+++ deadwood/deadwood_inference.py
@@ -1,35 +1,34 @@
-import torch
-from torchvision.transforms.functional import crop
 import json
 from os.path import join
-import safetensors.torch
-from common import *
-from .InferenceDataset import InferenceDataset
-import segmentation_models_pytorch as smp
 from pathlib import Path
-from torch.utils.data import DataLoader
-from tqdm import tqdm
+
 import numpy as np
+import safetensors.torch
+import segmentation_models_pytorch as smp
+import torch
+from torch.utils.data import DataLoader
+from torchvision.transforms.functional import crop
+from tqdm import tqdm
+
+from common import *
+
+from .InferenceDataset import InferenceDataset
 
 
-class DeadwoodInference():
-
+class DeadwoodInference:
     def __init__(self, config_path):
-
         # set float32 matmul precision for higher performance
-        torch.set_float32_matmul_precision('high')
+        torch.set_float32_matmul_precision("high")
 
-        with open(config_path, 'r') as f:
+        with open(config_path, "r") as f:
             self.config = json.load(f)
 
         self.model = None
-        self.device = torch.device(
-            "cuda" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         self.load_model()
 
     def load_model(self):
-
         if "segformer_b5" in self.config["model_name"]:
             model = smp.Unet(
                 encoder_name="mit_b5",
@@ -41,19 +40,21 @@ class DeadwoodInference():
             model = torch.compile(model)
             safetensors.torch.load_model(
                 model,
-                join(str(Path(__file__).parent.parent), "data",
-                     self.config["model_name"] + ".safetensors"))
+                join(
+                    str(Path(__file__).parent.parent),
+                    "data",
+                    self.config["model_name"] + ".safetensors",
+                ),
+            )
             # model = nn.DataParallel(model)
-            model = model.to(memory_format=torch.channels_last,
-                             device=self.device)
+            model = model.to(memory_format=torch.channels_last, device=self.device)
 
             model.eval()
 
             self.model = model
 
         else:
-            print("Invalid model name: ", self.config["model_name"],
-                  "Exiting...")
+            print("Invalid model name: ", self.config["model_name"], "Exiting...")
             exit()
 
     def inference_deadwood(self, input_tif):
@@ -63,12 +64,10 @@ class DeadwoodInference():
 
         # will always return a vrt, even when not reprojecting
         vrt_src = image_reprojector(
-            input_tif,
-            min_res=self.config["deadwood_minimum_inference_resolution"])
+            input_tif, min_res=self.config["deadwood_minimum_inference_resolution"]
+        )
 
-        dataset = InferenceDataset(image_src=vrt_src,
-                                   tile_size=1024,
-                                   padding=256)
+        dataset = InferenceDataset(image_src=vrt_src, tile_size=1024, padding=256)
 
         loader_args = {
             "batch_size": self.config["batch_size"],
@@ -79,26 +78,21 @@ class DeadwoodInference():
         inference_loader = DataLoader(dataset, **loader_args)
 
         outimage = np.zeros((dataset.height, dataset.width), dtype=np.float32)
-        for images, cropped_windows in tqdm(inference_loader,
-                                            desc="inference"):
-
-            images = images.to(device=self.device,
-                               memory_format=torch.channels_last)
+        for images, cropped_windows in tqdm(inference_loader, desc="inference"):
+            images = images.to(device=self.device, memory_format=torch.channels_last)
 
             output = None
             with torch.no_grad():
-
                 # if the the batch size is smaller than the configured batch size, apply padding
                 if images.shape[0] < self.config["batch_size"]:
                     pad = torch.zeros(
-                        (self.config["batch_size"], 3, 1024, 1024),
-                        dtype=torch.float32)
-                    pad[:images.shape[0]] = images
+                        (self.config["batch_size"], 3, 1024, 1024), dtype=torch.float32
+                    )
+                    pad[: images.shape[0]] = images
                     # move to device
-                    pad = pad.to(device=self.device,
-                                 memory_format=torch.channels_last)
+                    pad = pad.to(device=self.device, memory_format=torch.channels_last)
                     output = self.model(pad)
-                    output = output[:images.shape[0]]
+                    output = output[: images.shape[0]]
                 else:
                     output = self.model(images)
 
@@ -145,10 +139,11 @@ class DeadwoodInference():
                     maxy = outimage.shape[0]
 
                 # crop output tile to the correct size
-                output_tile = output_tile[:, diff_miny:output_tile.shape[1] -
-                                          diff_maxy,
-                                          diff_minx:output_tile.shape[2] -
-                                          diff_maxx]
+                output_tile = output_tile[
+                    :,
+                    diff_miny : output_tile.shape[1] - diff_maxy,
+                    diff_minx : output_tile.shape[2] - diff_maxx,
+                ]
 
                 # save tile to output array
                 outimage[miny:maxy, minx:maxx] = output_tile[0].numpy()
@@ -156,11 +151,10 @@ class DeadwoodInference():
         print("Postprocessing mask into polygons and filtering....")
 
         # threshold the output image
-        outimage = (outimage
-                    > self.config["probabilty_threshold"]).astype(np.uint8)
+        outimage = (outimage > self.config["probabilty_threshold"]).astype(np.uint8)
 
         # get nodata mask
-        nodata_mask = (vrt_src.dataset_mask() == 255)
+        nodata_mask = vrt_src.dataset_mask() == 255
 
         # mask out nodata in predictions
         outimage[~nodata_mask] = 0
@@ -171,12 +165,14 @@ class DeadwoodInference():
         # close the vrt
         vrt_src.close()
 
-        polygons = filter_polygons_by_area(polygons,
-                                           self.config["minimum_polygon_area"])
+        polygons = filter_polygons_by_area(
+            polygons, self.config["minimum_polygon_area"]
+        )
 
         # reproject the polygons back into the crs of the input tif
-        polygons = reproject_polygons(polygons, dataset.image_src.crs,
-                                      rasterio.open(input_tif).crs)
+        polygons = reproject_polygons(
+            polygons, dataset.image_src.crs, rasterio.open(input_tif).crs
+        )
 
         print("done")
 
